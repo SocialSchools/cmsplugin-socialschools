354ebdc2da4c88de3436b5459917ea465711953d
Pratik Vyas <m@pd.io>
2013-06-20 12:48:33 +0530

    production setup

---
+++ cmsplugin_socialschools/migrations/0001_initial.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'SocialSchools'
+        db.create_table('cmsplugin_socialschools', (
+            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+            ('socialschools_url', self.gf('django.db.models.fields.CharField')(max_length=125)),
+            ('community_id', self.gf('django.db.models.fields.IntegerField')()),
+            ('only_descendants', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('cmsplugin_socialschools', ['SocialSchools'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'SocialSchools'
+        db.delete_table('cmsplugin_socialschools')
+
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 6, 20, 0, 0)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'cmsplugin_socialschools.socialschools': {
+            'Meta': {'object_name': 'SocialSchools', 'db_table': "'cmsplugin_socialschools'", '_ormbases': ['cms.CMSPlugin']},
+            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'community_id': ('django.db.models.fields.IntegerField', [], {}),
+            'only_descendants': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'socialschools_url': ('django.db.models.fields.CharField', [], {'max_length': '125'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_socialschools']
