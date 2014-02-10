8d4328917b1f96839a045a3e065191948d8f9371
vinit Kumar <vinit.kumar@changer.nl>
2014-02-10 10:31:52 +0530

    cleanup code and add comments

--- cmsplugin_socialschools/static/cmsplugin_socialschools/js/socialschools_cmsplugin.js
+++ cmsplugin_socialschools/static/cmsplugin_socialschools/js/socialschools_cmsplugin.js
@@ -23,39 +23,47 @@ var singlePubPhotoTemplate = _.template($('#single-pub-photo-template').html());
 //     });
 // }
 
+
+/**
+ * [renderNews Function to render news headlines]
+ * @param  {[type]} selector [description]
+ * @param  {[type]} posts    [Post objects]
+ * @return {[type]}          [description]
+ */
 function renderNews(selector, posts) {
-   'use strict';
-    $(selector).find('.css-posts-content').empty();
-    _.each(posts.objects, function (post) {
-        var newsHtml = newsTemplate(post),
-            $post = $(document.createElement('div'));
-        $post.html(newsHtml);
-        $(selector).find('.news').append($post);     
-    });
-    $(".inline").colorbox({inline:true, width:"80%"});
+  'use strict';
+  $(selector).find('.css-posts-content').empty();
+  _.each(posts.objects, function (post) {
+    var newsHtml = newsTemplate(post),
+    $post = $(document.createElement('div'));
+    $post.html(newsHtml);
+    $(selector).find('.news').append($post);     
+  });
+  $(".inline").colorbox({inline:true, width:"80%"});
 }
 
+
 function renderPhotos($post, photos) {
-    'use strict';
-    _.each(photos.objects, function (photo) {
-        var photoHtml = photoTemplate(photo);
-        $post.find('.post-photos-container').append(photoHtml);
-    });
+  'use strict';
+  _.each(photos.objects, function (photo) {
+    var photoHtml = photoTemplate(photo);
+    $post.find('.post-photos-container').append(photoHtml);
+  });
 }
 
 function renderVideos($post, videos) {
-    _.each(videos.objects, function (video) {
-        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-        match = video.url.match(regExp);
-        if (match && match[7].length === 11) {
-            var iframe = $('<div class="video-thumbnail"><iframe width="560" height="315" src="//www.youtube.com/embed/' + match[7] +'" frameborder="0" allowfullscreen></iframe></div>');
-            $post.find('.post-videos-container').append(iframe);
-        }
-        else {
-            var video_link = $('<a href=' + video.url + '>' + video.url +'</a>');
-            $post.find('.post-videos-container').append(video_link);
-        }
-    });
+  _.each(videos.objects, function (video) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    match = video.url.match(regExp);
+    if (match && match[7].length === 11) {
+      var iframe = $('<div class="video-thumbnail"><iframe width="560" height="315" src="//www.youtube.com/embed/' + match[7] +'" frameborder="0" allowfullscreen></iframe></div>');
+      $post.find('.post-videos-container').append(iframe);
+    }
+    else {
+      var video_link = $('<a href=' + video.url + '>' + video.url +'</a>');
+      $post.find('.post-videos-container').append(video_link);
+    }
+  });
 }
 
 function renderPublicPhotos(selector, photos) {
@@ -65,7 +73,6 @@ function renderPublicPhotos(selector, photos) {
     var photoHTML = pubPhotoTemplate(photo);
       $(selector).find('.content-pub-photo').append(photoHTML);
   });
-  console.log(photos.objects[0]);
   var photo = photos.objects[0]
   var singlePhotoHTML = singlePubPhotoTemplate(photo);
   $(selector).find('.single-photo-container').append(singlePhotoHTML);
@@ -76,50 +83,55 @@ function renderPublicPhotos(selector, photos) {
   }
 }
 
+/**
+ * [renderPosts Render posts from the Socialschools API]
+ * @param  {[type]} selector [description]
+ * @param  {[type]} posts    [description]
+ * @return {[type]}          [description]
+ */
 function renderPosts(selector, posts) {
-    'use strict';
-    $(selector).find('.css-posts-content').empty();
+  'use strict';
+  $(selector).find('.css-posts-content').empty();
     _.each(posts.objects, function (post) {
-        var postHtml = postTemplate(post),
-            $post = $(document.createElement('div'));
-        $post.html(postHtml);
-        $(selector).find('.css-posts-content').append($post);
+      var postHtml = postTemplate(post),
+      $post = $(document.createElement('div'));
+      $post.html(postHtml);
+      $(selector).find('.css-posts-content').append($post);
 
-        // post.getComments(function (comments) {
-        //     renderComments($post, comments);
-        // });  
-        post.getPhotos(function (photos) {
-            renderPhotos($post, photos);
-        });
-        if (post._video_count) {
-            post.getVideos(function (videos) {
-            renderVideos($post, videos);
-        });
+      // post.getComments(function (comments) {
+      //     renderComments($post, comments);
+      // });  
+      post.getPhotos(function (photos) {
+        renderPhotos($post, photos);
+      });
+      if (post._video_count) {
+        post.getVideos(function (videos) {
+          renderVideos($post, videos);
+          });
         }
     });
     $(function () {
-        if (!posts.nextUrl) {
-            $(selector).find('a.css-posts-next-page').hide();
-        } else {
-            $(selector).find('a.css-posts-next-page').show();
-        }
-        if (!posts.prevUrl) {
-            $(selector).find('a.css-posts-prev-page').hide();
-        } else {
-            $(selector).find('a.css-posts-prev-page').show();
-        }
-       /** $(selector).find('a.css-posts-next-page').off('click').on('click', function (e) {
-           e.preventDefault();
-            posts.getNextPage(function (posts) {
-                renderPosts(selector, posts);
-            });
+      if (!posts.nextUrl) {
+        $(selector).find('a.css-posts-next-page').hide();
+      } else {
+        $(selector).find('a.css-posts-next-page').show();
+      }
+      if (!posts.prevUrl) {
+        $(selector).find('a.css-posts-prev-page').hide();
+      } else {
+        $(selector).find('a.css-posts-prev-page').show();
+      }
+      $(selector).find('a.css-posts-next-page').off('click').on('click', function (e) {
+        e.preventDefault();
+        posts.getNextPage(function (posts) {
+          renderPosts(selector, posts);
         });
-        $(selector).find('a.css-posts-prev-page').off('click').on('click', function (e) {
-            e.preventDefault();
-            posts.getPreviousPage(function (posts) {
-                renderPosts(selector, posts);
-            });
+      });
+      $(selector).find('a.css-posts-prev-page').off('click').on('click', function (e) {
+        e.preventDefault();
+        posts.getPreviousPage(function (posts) {
+          renderPosts(selector, posts);
         });
-       */
+      });
     });
 }
