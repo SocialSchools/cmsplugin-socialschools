2bad1549944b549ee7aeda0fad5f6b93500d846f
vinit Kumar <vinit.kumar@changer.nl>
2015-12-24 13:25:56 +0530

    add django 1.8 style migrations and bump version

--- cmsplugin_socialschools/migrations/0001_initial.py
+++ cmsplugin_socialschools/migrations/0001_initial.py
@@ -1,57 +1,33 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'SocialSchools'
-        db.create_table('cmsplugin_socialschools', (
-            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('socialschools_url', self.gf('django.db.models.fields.CharField')(max_length=125)),
-            ('community_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('only_descendants', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('cmsplugin_socialschools', ['SocialSchools'])
+    dependencies = [
+        ('cms', '0013_urlconfrevision'),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'SocialSchools'
-        db.delete_table('cmsplugin_socialschools')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 6, 20, 0, 0)'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        'cmsplugin_socialschools.socialschools': {
-            'Meta': {'object_name': 'SocialSchools', 'db_table': "'cmsplugin_socialschools'", '_ormbases': ['cms.CMSPlugin']},
-            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'community_id': ('django.db.models.fields.IntegerField', [], {}),
-            'only_descendants': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'socialschools_url': ('django.db.models.fields.CharField', [], {'max_length': '125'})
-        }
-    }
-
-    complete_apps = ['cmsplugin_socialschools']
+    operations = [
+        migrations.CreateModel(
+            name='SocialSchools',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('socialschools_url', models.CharField(help_text='the url to our API, fill out https://app.socialschools.nl/', max_length=125)),
+                ('community_id', models.IntegerField(help_text='the ID (number) of the community you want to display public posts of. You can find this number in the addressbar when you navigate to the community page. E.g. the ID of https://desocialschool.socialschools.nl/communities/306 is 306')),
+                ('only_descendants', models.BooleanField(help_text='This option will show public posts from any descendant community of the selected community.')),
+                ('community_and_descendants', models.BooleanField(help_text='This option will show public posts from Community and all it descendants.')),
+                ('only_headlines', models.BooleanField(help_text='This option will show  only headlines instead of full posts.')),
+                ('only_photos', models.BooleanField(help_text='This option will show only the images from the most recent published posts in a carousel')),
+                ('news_headlines', models.CharField(help_text='Fill out a title for the plugin in case you checked the only headlines option.', max_length=125, null=True, blank=True)),
+                ('headlines_with_thumbnails', models.BooleanField(help_text='This option will only show the headlines and a thumbail if there is an image or event in the post.', verbose_name='Only headlines and thumbnails')),
+                ('number_of_items', models.IntegerField(help_text='The number of items per page with a maximum of 10.', null=True, verbose_name='Number of items per page', blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+    ]
